926b2f5bb76c8c63110f91c422dae365b8e572c0
Eric Freeman <efreeman@1800contacts.com>
2015-05-31 15:31:18 -0600

    Added second interview test.

---
+++ InterviewProject.UnitTests/EvenFibonacciNumbersSumUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace InterviewProject.UnitTests
+{
+    [TestFixture]
+    public class EvenFibonacciNumbersSumUnitTests
+    {
+        private readonly EvenFibonacciNumbersSum _fizzbuzz;
+
+        public EvenFibonacciNumbersSumUnitTests()
+        {
+            _fizzbuzz = new EvenFibonacciNumbersSum();
+        }
+
+        [TestCase(10, 10)]
+        [TestCase(50, 44)]
+        [TestCase(100, 44)]
+        [TestCase(1000000, 1089154)]
+        [TestCase(4000000, 4613732)]
+        public void ItReturnsCorrectValue(int upperLimit, int expectedResult)
+        {
+            var result = _fizzbuzz.Execute(upperLimit);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ InterviewProject/EvenFibonacciNumbersSum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InterviewProject
+{
+    public class EvenFibonacciNumbersSum
+    {
+        /// <summary>
+        /// Calculate the sum of every even Fibonacci number from 0 to upperLimit
+        /// </summary>
+        public int Execute(int upperLimit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
